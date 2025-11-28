14632c1b3843e0aaf8cf22bda01cd38177ef4a8e
sasa00000 <126037771+sasa00000@users.noreply.github.com>
2025-11-28 21:33:38 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -81,7 +81,7 @@ def run_task(login_id, password, target_date):
 
     # ヘッドレスモード設定
     options = Options()
-    options.add_argument('--headless') 
+    options.add_argument('--headless=new') 
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
